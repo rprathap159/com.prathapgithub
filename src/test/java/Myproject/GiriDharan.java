0c021203771148de1a88e7752f18092fe14ff205
Giridharan56 <giridharanm31@gmail.com>
2025-05-06 14:54:54 +0530

    my Changes

---
+++ src/test/java/Myproject/GiriDharan.java
@@ -0,0 +1,8 @@
+package Myproject;
+
+public class GiriDharan {
+	public static void main(String[] args) {
+		System.out.println("mmmmmmmmmmmm");
+	}
+
+}
--- src/test/java/Myproject/PrathapName.java
+++ src/test/java/Myproject/PrathapName.java
@@ -3,7 +3,7 @@ package Myproject;
 public class PrathapName {
 
 	public static void main(String[] args) {
-		
+		System.out.println("gggggggg");
 
 	}
 
