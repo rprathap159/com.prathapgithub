1c4bbf2579778914853db0391d4e3accb227463e
rprathap159 <rprathap744600@gmail.com>
2025-05-06 15:12:29 +0530

    i added in login page & user page in the project

---
+++ src/test/java/Myproject/Login.java
@@ -0,0 +1,10 @@
+package Myproject;
+
+public class Login {
+
+	public static void main(String[] args) {
+		System.out.println("login is working");
+
+	}
+
+}
---
+++ src/test/java/Myproject/Userpage.java
@@ -0,0 +1,10 @@
+package Myproject;
+
+public class Userpage {
+
+	public static void main(String[] args) {
+   System.out.println("user is here in the page");
+
+	}
+
+}
